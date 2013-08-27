20cada7534853715e1e99fd83ac81b121d28c52a
Ron Rahaman <rahaman@cs.uchicago.edu>
2013-08-27 15:38:47 -0500

    read/write completes without segfault in v0

--- v0/read_write_driver.f90
+++ v0/read_write_driver.f90
@@ -3,7 +3,7 @@ program read_write_driver
   use read_xml_primitives
   use write_xml_primitives
   use xmlparse
-  use xml_data_geometry_t
+  use xml_data_materials_t
 
   implicit none
 
@@ -12,9 +12,9 @@ program read_write_driver
   call get_command_argument(1, mode)
 
   select case(trim(mode))
-  case("geometry")
-    print *, "*** calling read_write_geometry() ..."
-    call read_write_geometry()
+  case("materials")
+    print *, "*** calling read_write_materials() ..."
+    call read_write_materials()
   case default
     print *, "Error from read_write_driver"
     print *, "Must provide command line argument for testing mode: ('geometry')"
@@ -23,16 +23,16 @@ program read_write_driver
 
   contains
 
-    subroutine read_write_geometry()
+    subroutine read_write_materials()
       integer :: tstart, tend, trate
 
       call system_clock(tstart, trate)
-      call read_xml_file_geometry_t('geometry.xml')
+      call read_xml_file_materials_t('materials.xml')
       call system_clock(tend)
-      print *, 'read_xml_file_geometry_t completed in (seconds):', &
+      print *, 'read_xml_file_materials_t completed in (seconds):', &
         dble(tend-tstart)/trate
-      !call write_xml_file_geometry_t('geometry_driver_output.xml')
-
-    end subroutine read_write_geometry
+      call write_xml_file_materials_t('materials_validation.xml')
+    end subroutine read_write_materials
+      
 
 end program read_write_driver
